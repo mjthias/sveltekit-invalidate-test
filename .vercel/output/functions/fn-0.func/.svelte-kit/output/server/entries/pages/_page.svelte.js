763ae9c0b1081006246088e9e45506be737e647d
Mathias Dahl Jensen <mathias.j@forteadvice.com>
2023-09-03 20:44:19 +0200

    rm vercel files

--- .vercel/output/functions/fn-0.func/.svelte-kit/output/server/entries/pages/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<h1 data-svelte-h="svelte-yvbimo">Cute dogs</h1> <p data-svelte-h="svelte-st104t">Click a link to see a cute dog</p>`;
-});
-export {
-  Page as default
-};
--- .vercel/output/functions/fn-0.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.98c9d49d.js","_app/immutable/chunks/scheduler.63274e7e.js","_app/immutable/chunks/index.71095f26.js","_app/immutable/chunks/singletons.891ee3d9.js"];
-export const stylesheets = [];
-export const fonts = [];
